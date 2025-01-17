4c738f5d4b62c4dfb3fb71939dc854c6dfb3f131
FernandoNLopez <lopezfer1998@outlook.com>
2025-01-16 21:34:52 -0300

    "first commit"

---
+++ app/src/main/java/com/learning/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,24 @@
+package com.learning.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.learning.horoscapp.R
+import com.learning.horoscapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding:FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
